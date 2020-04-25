6b14deecf3cce00f5705abf30010bfa9c57e068b
gourav chandna <gouravchandna1998@gmail.com>
2020-04-26 02:14:47 +0530

    project setup+static pages+forms validation done

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Code4Share - a platform for sharing code' });
+});
+router.get('/about',function(req,res,next){
+  res.render('about',{title:'Code4Share - a platform for sharing code'})  
+});
+
+router.route('/contact')
+.get(function(req,res,next){
+  res.render('contact',{title:'Code4Share - a platform for sharing code.'});
+})
+.post(function(req,res,next){
+  req.checkBody('name','Invalid name').notEmpty();
+  req.checkBody('email','Invalid email').isEmail();
+  req.checkBody('message','Empty message').notEmpty();
+  var errors=req.validationErrors();
+  if(errors){
+    res.render('contact',{
+      title:'Code4Share - a platform for sharing code.',
+      name:req.body.name,
+      email:req.body.email,
+      message:req.body.message,
+      errorMessages:errors
+    });
+  }else{
+res.render('thank',{ title:'Code4Share -a platform for sharing code.'});  
+}
+});
+
+module.exports = router;
