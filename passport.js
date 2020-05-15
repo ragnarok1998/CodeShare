8ff19ba0df25c150a3e91aa37c48f54ed31c42b8
gourav chandna <gouravchandna1998@gmail.com>
2020-05-15 11:33:50 +0530

    sign up/sign authentication with facebook done.

--- passport.js
+++ passport.js
@@ -1,6 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
+var FacebookStrategy = require('passport-facebook').Strategy;
 passport.serializeUser(function (user, done) {
     done(null, user._id);
 });
@@ -31,4 +31,40 @@ passport.use(new LocalStrategy({
             return done(null, user);
         })
     }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: '240152484067830',
+    clientSecret: '435052e837dfb67da3f042bf91bda729',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'email']
+},
+    function (token, refreshToken, profile, done) {
+        User.findOne({ 'facebookId': profile.id }, function (err, user) {
+            if (err) return done(err);
+
+            if (user) {
+                return done(null, user);
+            } else {
+                User.findOne({ email: profile.emails[0].value }, function (err, user) {
+                    if (user) {
+                        user.facebookId = profile.id
+                        return user.save(function (err) {
+                            if (err) return done(null, false, { message: "can't save user info" });
+                            return done(null, user);
+                        })
+                    }
+                    var user = new User();
+                    user.name = profile.displayName;
+                    user.email = profile.emails[0].value;
+                    user.facebookId = profile.id
+                    user.save(function (err) {
+                        if (err) return done(null, false, { message: "can't save user info" });
+                        return done(null, user);
+                    });
+                })
+            }
+        });
+    }
+
 ));
--- routes/auth.js
+++ routes/auth.js
@@ -50,4 +50,12 @@ router.route('/register')
         req.logout();
         res.redirect('/');
     });
+
+    router.get('/auth/facebook',passport.authenticate('facebook',{scope:'email'}));
+
+router.get('/auth/facebook/callback',passport.authenticate('facebook',{
+successRedirect:'/',
+failureRedirect:'/'
+}));
+
 module.exports = router;
